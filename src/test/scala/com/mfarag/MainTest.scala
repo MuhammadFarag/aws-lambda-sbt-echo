7fcd898ce663e667e8cccd0fa19c37e56170dbc1
Muhammad Farag <MuhammadFarag@gmail.com>
2018-04-12 21:27:36 -0400

    Capitalize a message received in JSON format. Purpose is to receive a JSON message, parse the message do something and serialize the result, as a workflow.

--- src/test/scala/com/mfarag/MainTest.scala
+++ src/test/scala/com/mfarag/MainTest.scala
@@ -6,13 +6,13 @@ import org.scalatest.{FunSuite, Matchers}
 
 class MainTest extends FunSuite with Matchers {
 
-  test("Echo returns whatever it receives") {
-    val input: InputStream = new ByteArrayInputStream("Text".getBytes("UTF-8"))
+  test("Process returns the upper case of the message it receives") {
+    val input: InputStream = new ByteArrayInputStream("""{"s":"hello world"}""".getBytes("UTF-8"))
     val output = new ByteArrayOutputStream()
 
-    new Main().echo(input,output)
+    new Main().process(input, output)
 
-    output.toString("UTF-8") shouldBe "Text"
+    output.toString("UTF-8") shouldBe """{"s":"HELLO WORLD"}"""
   }
 
 }
